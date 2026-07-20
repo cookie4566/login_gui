507ca11e96b6130416c569814cb6c51afead5d78
cookie4566 <kennedyc416@gmail.com>
2026-07-19 20:26:41 -0400

    Basic window features

--- login.py
+++ login.py
@@ -1,6 +1,10 @@
 from tkinter import *
 from tkinter import ttk
 
+# function for saving login info
+
+# function for signing up
+
 # Window for login information
 window = Tk()
 frame = ttk.Frame(window, padding = 5)
@@ -16,4 +20,7 @@ ttk.Entry(frame).grid(column=1, row=1)
 
 #exit button
 ttk.Button(frame, text="quit",command=window.destroy).grid(column=1, row=2)
+ttk.Button(frame, text="Sign Up").grid(column=2, row=2)
+ttk.Button(frame, text="Log In",command=window.destroy).grid(column=0, row=2)
+
 window.mainloop()
